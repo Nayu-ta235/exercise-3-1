19052546a754f3b5a21f131c3320312f86e0e88e
Nayu-ta235 <mdarwish@pelajar.kkhse.edu.my>
2026-03-17 12:02:49 +0800

    Add files via upload

---
+++ exercise 3.1.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title("Welcome GUI App")
+st.text("This is my first Graphical Interface")
+
+name = st.text_input("Enter your name:")
+
+num1 = st.number_input("Enter first number:", value=0)
+num2 = st.number_input("Enter second number:", value=0)
+
+show_greeting = st.checkbox("Show Greeting")
+
+if st.button("Submit"):
+
+    if name:
+        st.write(f"Hello, {name}!")
+    else:
+        st.write("Hello, stranger!")
+    
+    st.write(f"The sum of two number is {num1 + num2}.")
+    
+
+    if show_greeting:
+        st.write("Have a nice day!")
